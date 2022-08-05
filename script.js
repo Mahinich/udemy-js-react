0b838d1026b4479a101ae97447a7d3633ddb6239
Mahinich <andrey.mahinitch@gmail.com>
2022-08-05 23:56:32 +0300

    triangular

--- script.js
+++ script.js
@@ -55,4 +55,14 @@
 //   }
 // }
 
+// let result = '';
+// let length = 9;
 
+// for (let i = 1; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += '*';
+//   }
+//   result += '\n';
+// }
+
+console.log(result);
