41097cd39eee0f4a926e460e06914e0d78054377
Mahinich <andrey.mahinitch@gmail.com>
2022-08-05 23:24:29 +0300

    loop in loop

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// let num = 50;
+
+// while (num < 55) {
+  // console.log(num);
+  // ++num;
+// }
+
+// do {
+  // console.log(num);
+  // num++;
+// } while (num < 49);
+
+// for (let i = 1; i < 10; i++) {
+//  if (num === 53) {
+  // continue;
+//  }
+//  console.log(num);
+//  num++;
+// }
+
+// let text = '';
+
+// for (let i = 10; i <= 100; i += 10) {
+  // text += `The number is ${i} <br/>`;
+// }
+
+// document.querySelector('p').innerHTML = text;
+
+
+// const phones = ['iphone', 'oppo','a1', 'huawei', 'redmi 10'];
+// 
+// let i = 0,
+  // len = phones.length,
+  // text = '';
+
+// for (; i < len; i++) {
+//   text += `${phones[i]} <br/>`;
+// }
+
+// document.querySelector('p').innerHTML = text;
+
+// for (; i < len; ) {
+  // text += `${phones[i]} <br/>`;
+  // i++;
+// }
+// 
+// document.querySelector('p').innerHTML = text;
+
+// for (let p = 0; p < 3; p++) {
+//   // console.log(`${p} ${o}`);
+//   for (let o = 0; o < 3; o++) {
+//     console.log(`${p} ${o}`);
+//   }
+// }
+
+
