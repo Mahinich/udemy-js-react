d7fd901ade4ccb7cfc497d37e85ed97a6a8b6ead
Mahinich <andrey.mahinitch@gmail.com>
2022-08-05 09:16:25 +0300

    more info

---
+++ script.js
@@ -0,0 +1,47 @@
+// const hamburger = 1;
+// const  fried = 1;
+// 
+// if (hamburger && fried) {
+  // console.log('satisfied');
+// } else {
+  // console.log('hungry');
+// }
+
+// const hamburger = 3;
+// const fried = 1;
+// const cola = 1;
+
+// if (hamburger === 3 && cola ===1 && fried ===1) {
+//   console.log('satisfied');
+// } else {
+//   console.log('hungry');
+// }  
+// console.log(hamburger === 3 && cola  && fried === 1);
+// console.log(1 && 5);
+// console.log(1 && 0);
+// console.log(null && 'a');
+// console.log(1 && 'a');
+
+// const hamburger = 3;
+// const fried = 1;
+// const cola = 0;
+
+// if (hamburger === 3 || cola ===1 || fried ===1) {
+//   console.log('satisfied');
+// } else {
+//   console.log('hungry');
+// }  
+// console.log(hamburger === 3 || cola || fried === 1);
+
+// const hamburger = 2;
+// const fried = null;
+// const cola = 1;
+// const nuggets = 2;
+// 
+// if (hamburger === 2 && fried === 1 || cola === 1 && nuggets) {
+  // console.log('good');
+// } else {
+  // console.log('not good');
+// }
+// 
+// console.log(hamburger === 2 && fried === 1 || cola === 1 && nuggets);
