e56b9932ca934fe09c12a04f56401befe7eef54e
Mahinich <andrey.mahinitch@gmail.com>
2022-08-12 08:39:11 +0300

    point 24 is done

--- script.js
+++ script.js
@@ -1,99 +1,47 @@
 'use strict';
+const numberOfFilms = +prompt('How many films have you watched?', '');
 
-// let num = 50;
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: [ ],
+   actors: { },
+   genres: [ ],
+   privat: false,
+};
 
-// while (num < 55) {
-  // console.log(num);
-  // ++num;
-// }
+/* const firstQuestion = prompt('Pick oine of the last watched films'),
+     firstAnswer = prompt('Rate it', ''),
+     secondQuestion = prompt('Pick oine of the last watched films'),
+     secondAnswer = prompt('Rate it', '');
 
-// do {
-  // console.log(num);
-  // num++;
-// } while (num < 49);
+personalMovieDB.movies[firstQuestion] = firstAnswer;      
+personalMovieDB.movies[secondQuestion] = secondAnswer;   */ 
+let i = 0;
 
-// for (let i = 1; i < 10; i++) {
-//  if (num === 53) {
-  // continue;
-//  }
-//  console.log(num);
-//  num++;
-// }
+do {
+   i++;
 
-// let text = '';
+   const film = prompt('Pick one of the last watched films'),
+   rating = prompt('Rate it', '');
+   
+   if (film !== null && rating !== null && film !== '' && rating !== '' && film.length < 50) {
+      personalMovieDB.movies[film] = rating; 
+      console.log('done');     
+   } else {
+      console.log('error');
+      i--;
+   }
 
-// for (let i = 10; i <= 100; i += 10) {
-  // text += `The number is ${i} <br/>`;
-// }
+} while (i < 1);
 
-// document.querySelector('p').innerHTML = text;
-
-
-// const phones = ['iphone', 'oppo','a1', 'huawei', 'redmi 10'];
-// 
-// let i = 0,
-  // len = phones.length,
-  // text = '';
-
-// for (; i < len; i++) {
-//   text += `${phones[i]} <br/>`;
-// }
-
-// document.querySelector('p').innerHTML = text;
-
-// for (; i < len; ) {
-  // text += `${phones[i]} <br/>`;
-  // i++;
-// }
-// 
-// document.querySelector('p').innerHTML = text;
-
-// for (let p = 0; p < 3; p++) {
-//   // console.log(`${p} ${o}`);
-//   for (let o = 0; o < 3; o++) {
-//     console.log(`${p} ${o}`);
-//   }
-// }
-
-// let result = '';
-// let length = 9;
-
-// for (let i = 1; i < length; i++) {
-//   for (let j = 0; j < i; j++) {
-//     result += '*';
-//   }
-//   result += '\n';
-// }
-
-// console.log(result);
-
-// for (let line = "#"; line.length < 8; line += "#")
-//   console.log(line);
-
-// let size = 8;
-
-// let board = "";
-
-// for (let y = 0; y < size; y++) {
-//   for (let x = 0; x < size; x++) {
-//     if ((x + y) % 2 == 0) {
-//       board += " ";
-//     } else {
-//       board += "#";
-//     }
-//   }
-//   board += "\n";
-// }
-
-// console.log(board);
-
-// first: for (let i = 0; i < 3; i++)  {
-//   console.log(`first level ${i}`);
-//   for (let j = 0; j < 3; j++)  {
-//     console.log(`second level ${j}`);
-//     for (let k = 0; k < 5; k++)  {
-//       if (k === 2) continue first;
-//       console.log(`third level ${k}`);
-//     }
-//   }
-// }
+   if (personalMovieDB.count < 10) {
+      alert('Not many films are watched');
+   } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      alert('Not a little quantity are watched')
+   } else if (personalMovieDB.count > 30) {
+      alert('FILM-LOVER');
+   } else {
+      alert('ERROR');
+   }
+      
+console.log(personalMovieDB);
