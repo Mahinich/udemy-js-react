234e1eeca17045232691645520768c2ba3818af9
Mahinich <andrey.mahinitch@gmail.com>
2022-09-10 11:55:16 +0300

    adds

--- script.js
+++ script.js
@@ -166,3 +166,34 @@ function reverse() {
 }
 
 reverse(); */
+
+/* triangular figure */
+
+/* const lines = 5;
+let result = '';
+
+for(let i = 1; i <= lines; i++) {
+   for(let j = 0; j < i; j++) {
+      result += '*';
+   }
+   result += '\n';
+}
+
+console.log(result); */
+
+/* triangular figure after task 23 */
+
+/* const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+  for (let j = 0; j < lines - i; j++) {
+      result += " ";
+  }
+  for (let j = 0; j < 2 * i + 1; j++) {
+      result += "*";
+  }
+  result += "\n";
+}
+
+console.log(result) */
