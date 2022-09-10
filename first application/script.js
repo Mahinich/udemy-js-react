d38068e63cea081ab0329ed6f36962198a8fb211
Mahinich <andrey.mahinitch@gmail.com>
2022-09-10 16:17:04 +0300

    apply loops and conditions 24

--- first application/script.js
+++ first application/script.js
@@ -14,19 +14,34 @@
  const numberOfFilms = +prompt('How many films have you watches?', '');
 
  const personalMovieDB = {
-    count: numberOfFilms,
-    movies: { },
-    actors: { },
-    genres: [ ],
-    privat: false,
+  count: numberOfFilms,
+  movies: { },
+  actors: { },
+  genres: [ ],
+  privat: false,
 };
 
-const firstQuestion1 = prompt('Pick oine of the last watched films'),
-      secondQuestion1 = prompt('Rate it', ''),
-      firstQuestion2 = prompt('Pick oine of the last watched films'),
-      secondQuestion2 = prompt('Rate it', '');
+for (let i = 0; i < 2; i++) {
+  firstQuestion = prompt('Pick oine of the last watched films', ''),
+  secondQuestion = prompt('Rate it', '');
+  
+  if (firstQuestion === null || firstQuestion === '' || firstQuestion.length > 10 || secondQuestion === null || secondQuestion === '') {
+    console.log('error');
+    i--;
+  } else {
+    personalMovieDB.movies[firstQuestion] = secondQuestion;  
+    console.log('done');
+  }
+}
 
-personalMovieDB.movies[firstQuestion1] = secondQuestion1;      
-personalMovieDB.movies[firstQuestion2] = secondQuestion2;   
+if (personalMovieDB.count <= 10) {
+  console.log('little quantity');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <=50) {
+  console.log('medium quantity');
+} else if (personalMovieDB.count > 50) {
+  console.log('high quantity');
+} else {
+  console.log ('something wrong');
+}
 
 console.log(personalMovieDB);
