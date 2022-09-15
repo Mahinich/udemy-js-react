38afea0b5a5622643e3f03653c9047c98522e7d5
Mahinich <andrey.mahinitch@gmail.com>
2022-09-15 21:42:59 +0300

    point 29 is done/first appication folder/

--- first application/script.js
+++ first application/script.js
@@ -11,37 +11,79 @@
 // let firstAnswer = prompt('How many films have have you seen?');
 // let secondAnswer = prompt('Give your rate');
 
- const numberOfFilms = +prompt('How many films have you watches?', '');
+ let numberOfFilms;
 
- const personalMovieDB = {
-  count: numberOfFilms,
-  movies: { },
-  actors: { },
-  genres: [ ],
-  privat: false,
-};
-
-for (let i = 0; i < 2; i++) {
-  firstQuestion = prompt('Pick oine of the last watched films', ''),
-  secondQuestion = prompt('Rate it', '');
+function go() {
+  function start () {
+    numberOfFilms = Number(prompt('How many films have you watches?', ''));
   
-  if (firstQuestion === null || firstQuestion === '' || firstQuestion.length > 10 || secondQuestion === null || secondQuestion === '') {
-    console.log('error');
-    i--;
-  } else {
-    personalMovieDB.movies[firstQuestion] = secondQuestion;  
-    console.log('done');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('How many films have you watches?', '');
+    }
   }
-}
+  
+  start();
+  
+   const personalMovieDB = {
+    count: numberOfFilms,
+    movies: { },
+    actors: { },
+    genres: [ ],
+    privat: false,
+  };
+  
+  function survey() {
+    for (let i = 0; i < 2; i++) {
+      firstQuestion = prompt('Pick oine of the last watched films', ''),
+      secondQuestion = prompt('Rate it', '');
+      
+      if (firstQuestion === null || firstQuestion === '' || firstQuestion.length > 10 || secondQuestion === null || secondQuestion === '' || isNaN(secondQuestion)) {
+        console.log('error');
+        i--;
+      } else {
+        personalMovieDB.movies[firstQuestion] = secondQuestion;  
+        console.log('done');
+      }
+    }
+  }
+  
+  survey();
+  
+  function level() {
+    if (personalMovieDB.count <= 10) {
+      console.log('little quantity');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count <=50) {
+      console.log('medium quantity');
+    } else if (personalMovieDB.count > 50) {
+      console.log('high quantity');
+    } else {
+      console.log ('something wrong');
+    }
+  }
+  
+  level();
+  
+  function showMyDB(hidden) {
+    if(!hidden) {
+      console.log(personalMovieDB);
+    } else {
+      console.log('privat DB');
+    }
+  }
+  
+  showMyDB(personalMovieDB.privat); 
+  
+  function favouriteGenres() {
+    const genres = new Array();
+   for (let i = 1; i <= 3; i++) {
+    let query = prompt(`Ваш улюблений жанр під номером ${i}?`);
+    genres[i-1] = query;
+   } 
+   console.log(genres);
+  }
+  
+  favouriteGenres();
+} 
 
-if (personalMovieDB.count <= 10) {
-  console.log('little quantity');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count <=50) {
-  console.log('medium quantity');
-} else if (personalMovieDB.count > 50) {
-  console.log('high quantity');
-} else {
-  console.log ('something wrong');
-}
-
-console.log(personalMovieDB);
+go();
+// console.log(personalMovieDB);
