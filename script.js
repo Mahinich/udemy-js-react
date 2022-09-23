4da6e137fad2bd5e65847aef359b40665a68c0e5
Mahinich <andrey.mahinitch@gmail.com>
2022-09-23 10:01:21 +0300

    poin 32, time 20:52

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 'use strict';
 
-const options = {
+
+/* const options = {
   name: 'Andrii',
   height: 185,
   age: 30,
   colors: {
     border: 'black',
     background: 'red',
+  },
+  makeTEst: function() {
+    console.log('Tttessst');
   }
 };
 
-for (let key in options) {
-  console.log(key + ' >>> ' + options[key]);
+options.makeTEst();
+console.log( Object.values(options) ); 
+
+const h1 = document.querySelector('h1'); //const for diversity
+
+h1.innerHTML = Object.values(options).length
+document.write( Object.values(options) );
+
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof(options[key]) === 'object') {
+//     for (let jey in options[key]) {
+//       console.log(`Властивість ${jey} яка має значення ${options[key][jey]} \`);
+//       counter++
+//     }
+//   } else {
+//   console.log(key + ' ———> ' + options[key]);
+//   counter++
+//   }
+// }
+
+// console.log(counter); */
+
+/* const obj = {
+  a: 1,
+  b: undefined,
+  c: true,
+  d: null,
+  e: {
+    'first value': 'one',
+    'second value': 'two',
+  }
+};
+
+
+for (let key in obj) {
+  console.log(`${key} : ${obj[key]}`);
+  if (typeof(obj[key]) === 'object') {
+    for (let i in obj[key]) {
+      console.log('властивість ' + i + ' має значення' + ' ' + obj[key][i]);
+    }
+  }
 }
 
-console.log(options['colors']['background']);
+console.log(obj.e['first value']);
+console.log(Object.keys(obj));
+console.log(Object.values(obj)); */
