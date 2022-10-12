8e696f2ad200837f071e692a2483a533b8949ae2
Mahinich <andrey.mahinitch@gmail.com>
2022-10-12 22:07:29 +0300

    functions are applied as methods to obj personalMovieDB

--- first application/script.js
+++ first application/script.js
@@ -10,80 +10,74 @@
 
 // let firstAnswer = prompt('How many films have have you seen?');
 // let secondAnswer = prompt('Give your rate');
+  
+const personalMovieDB = {
+	count: 0,
+	movies: { },
+	actors: { },
+	genres: [ ],
+	privat: false,
+	start: function() {
+		personalMovieDB.count = +prompt('How many films have you watches?', '');
 
- let numberOfFilms;
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+		personalMovieDB.count = +prompt('How many films have you watches?', '');
+		}
+	},
+	survey: function() {
+		for (let i = 0; i < 2; i++) {
+			firstQuestion = prompt('Pick oine of the last watched films', ''),
+			secondQuestion = prompt('Rate it', '');
+			
+			if (firstQuestion === null || firstQuestion === '' || firstQuestion.length > 10 || secondQuestion === null || secondQuestion === '' || isNaN(secondQuestion)) {
+				console.log('error');
+				i--;
+			} else {
+				personalMovieDB.movies[firstQuestion] = secondQuestion;  
+				console.log('done');
+			}
+		}
+	},  
+	level: function() {
+		if (personalMovieDB.count <= 10) {
+			console.log('little quantity');
+		} else if (personalMovieDB.count > 10 && personalMovieDB.count <=50) {
+			console.log('medium quantity');
+		} else if (personalMovieDB.count > 50) {
+			console.log('high quantity');
+		} else {
+			console.log ('something wrong');
+		}
+	},
+	showMyDB: function(hidden) {
+		if(!hidden) {
+			console.log(personalMovieDB);
+		} else {
+			console.log('privat DB');
+		}
+	},
+	toggleVisibleDB: function() {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false
+		} else {
+			personalMovieDB.privat = true
+		}
+	},
+	favouriteGenres: function() {
+			for (let i = 1; i < 2; i++) {
+				let query = prompt(`Введіть ваші улюблені жанри через кому`).toLocaleLowerCase();
 
-function go() {
-  function start () {
-    numberOfFilms = Number(prompt('How many films have you watches?', ''));
-  
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('How many films have you watches?', '');
-    }
-  }
-  
-  start();
-  
-   const personalMovieDB = {
-    count: numberOfFilms,
-    movies: { },
-    actors: { },
-    genres: [ ],
-    privat: false,
-  };
-  
-  function survey() {
-    for (let i = 0; i < 2; i++) {
-      firstQuestion = prompt('Pick oine of the last watched films', ''),
-      secondQuestion = prompt('Rate it', '');
-      
-      if (firstQuestion === null || firstQuestion === '' || firstQuestion.length > 10 || secondQuestion === null || secondQuestion === '' || isNaN(secondQuestion)) {
-        console.log('error');
-        i--;
-      } else {
-        personalMovieDB.movies[firstQuestion] = secondQuestion;  
-        console.log('done');
-      }
-    }
-  }
-  
-  survey();
-  
-  function level() {
-    if (personalMovieDB.count <= 10) {
-      console.log('little quantity');
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count <=50) {
-      console.log('medium quantity');
-    } else if (personalMovieDB.count > 50) {
-      console.log('high quantity');
-    } else {
-      console.log ('something wrong');
-    }
-  }
-  
-  level();
-  
-  function showMyDB(hidden) {
-    if(!hidden) {
-      console.log(personalMovieDB);
-    } else {
-      console.log('privat DB');
-    }
-  }
-  
-  showMyDB(personalMovieDB.privat); 
-  
-  function favouriteGenres() {
-    const genres = new Array();
-   for (let i = 1; i <= 3; i++) {
-    let query = prompt(`Ваш улюблений жанр під номером ${i}?`);
-    genres[i-1] = query;
-   } 
-   console.log(genres);
-  }
-  
-  favouriteGenres();
-} 
+			if (query == null || query == ' ') {
+				console.log('Incorect data');
+				i--;
+			} else {
+				personalMovieDB.genres = query.split(', ').sort();
+			}
+			console.log(personalMovieDB.genres);
+		} 
 
-go();
-// console.log(personalMovieDB);
+		personalMovieDB.genres.forEach((item, i) => {
+			console.log(`Favourite genre number ${i + 1} is ${item}`);
+		});
+	}
+};
