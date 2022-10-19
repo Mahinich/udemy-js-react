300ff9b183075a1214154b262dc46d685b7008d4
Mahinich <andrey.mahinitch@gmail.com>
2022-10-20 02:11:37 +0300

    function return function

--- script.js
+++ script.js
@@ -1,97 +1,33 @@
-/* const obj = {
-  a: 5,
-  b: 'text',
-};
- */
+'use strict';
 
-//--------------------------------
+/* let number = 5; debugger
 
-
-function copy(mainObj) {
-  let copyObj = {};
-
-  let key;
-
-  for (key in mainObj) {
-    copyObj[key] = mainObj[key]; 
-  }
-
-  return copyObj;
+function logNumber() {
+  console.log(number); debugger
 }
 
-/* const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4,
+number = 6;
+
+logNumber(); debugger
+
+number = 9;
+
+logNumber(); debugger */
+
+function createCounter() {
+  let counter = 0;
+
+  const myFunction = function() { debugger
+    counter += 1; debugger
+    return counter; debugger
   }
-};
 
-const newNumbers = copy(numbers); 
-
-newNumbers.a = 8
-newNumbers.b = 'string'
-newNumbers.c.y = 'chotiri'
-
-console.log(newNumbers);
-console.log(numbers); */
-
-//--------------------------------
-
-/* const addValues = {
-  d: 3 + '33',
-  e: 14 / '2',
-};
-
-const newObj = Object.assign(newNumbers, addValues, {name: 'Andriy', surname: 'Mahinich'});
-console.log(newNumbers);
-
-const cloneNewObj = Object.assign({},  {name: 'Andriy', surname: 'Mahinich'});
-console.log(cloneNewObj); */
-
-//--------------------------------
-
-/* const oldArr = ['a', 'Mahi', 'Nata'];
-const newArr = oldArr.slice();
-
-console.log(oldArr);
-console.log(newArr);
-
-newArr[1] = 'Mahinich'
-console.log(newArr); */
-
-//--------------------------------
-
-/* const films = ['terminator', 'wolverine', 'hellboy'],
-      platforms = ['wordpress', 'opencart', 'tilda'],
-      all = [...films, ...platforms];
-
-console.log(all);  */     
-
-//--------------------------------
-
-/* function log(a, b, c, d) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-  console.log(d);
+  return myFunction;
 }
+debugger
+const increment = createCounter(); debugger
+const c1 = increment(); debugger
+const c2 = increment(); debugger
+const c3 = increment(); debugger
 
-const num = [1, 3, 5, 7];
-log(...num); */
-
-//--------------------------------
-
-/* const obj = {
-  one: 1,
-  two: 2,
-};
-
-const anotherObj = {...obj};
-
-console.log(anotherObj);
-
-anotherObj['new poperty'] = 'uraaa';
-
-console.log(anotherObj); */
+console.log(c1, c2, c3);
