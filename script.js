1443e575770d16c46f32958ff6ed772f1cc4c8f4
Mahinich <andrey.mahinitch@gmail.com>
2022-10-30 13:44:53 +0200

    point 43 is upworked

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 'use strict';
 
-const box = document.getElementById('box');
-console.log(box);
+const box      = document.getElementById('box');
+const btns     = document.getElementsByTagName('button');
+const circles  = document.getElementsByClassName('circle');
+const hearts   = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('div');
+const wrapper  = document.querySelector('.wrapper');
 
-const btns = document.getElementsByTagName('button');
-console.log(btns);
+/* box.style.backgroundColor = 'pink';
+box.style.width = '400px'; */
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+box.style.cssText = 'background-color: aqua; width: 150px; cursor: pointer;'
 
-const hearts = document.querySelectorAll('.heart');
+btns[2].style.borderRadius = '25px 5px 25px 5px';
 
-hearts.forEach(item => {
-  console.log(item);
+circles[0].style.backgroundColor = 'green';
+
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'yellow';
+// }
+
+hearts.forEach(element => {
+  element.style.backgroundColor = 'pink';
 })
 
-const oneHeart = document.querySelector('div');
-console.log(oneHeart);
+const div = document.createElement('div');
+const textNode = document.createTextNode('Andriy');
+
+div.classList.add('black');
+
+// document.body.append(div);
+document.body.prepend(div);
+div.innerHTML = '<h1>Andriy</h1>';
+
+div.insertAdjacentHTML('afterend', '<h2>And</h2>');
+
+// div.textContent = 'Mahinich';
+
+// wrapper.append(div);
+// wrapper.prepend(div);
+
+// btns[1].before(div);
+// btns[1].after(div);
+
+// circles[0].remove();
+
+// hearts[1].replaceWith(circles[1]);
+
+// wrapper.insertBefore(btns[2], hearts[1]); //derprecated
+// wrapper.removeChild(hearts[1]); //derprecated
+// hearts[0].replaceWith(btns[3]);
+// wrapper.replaceChild(circles[0], hearts[2]); //derprecated
