0ab45a54c31f3d3877a47b905657bc2ccd6fe279
Mahinich <andrey.mahinitch@gmail.com>
2022-11-13 14:11:16 +0200

    point 48 is upworked

--- project 2/js/script.js
+++ project 2/js/script.js
@@ -14,50 +14,96 @@
 
 'use strict';
 
-const movieDB = {
+document.addEventListener('DOMContentLoaded', () => {
+  const movieDB = {
     movies: [
-        "Andriy",
-        "Natali",
-        "Mahinich",
-        "Family",
-        "Happy"
+      "Andriy",
+      "Natali",
+      "Mahinich",
+      "Family",
+      "Happy"
     ]
-};
+  };
+  
+  const annoyingAd  = document.querySelectorAll('.promo__adv img');
+  const genre       = document.querySelector('.promo__genre');
+  const bg          = document.querySelector('.promo__bg');
+  const listOfFilms = document.querySelector('.promo__interactive-list');
+  const form        = document.querySelector('form.add');
+  const inputField  = form.querySelector('.adding__input');
+  const checkbox    = form.querySelector('[type="checkbox"]');
+  const submit      = form.querySelector('button');
 
-const annoyingAd = document.querySelectorAll('.promo__adv img');
-const genre = document.querySelector('.promo__genre');
-const bg = document.querySelector('.promo__bg');
-const listOfFilms = document.querySelector('.promo__interactive-list');
+  form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
 
-const deleteListener = (e) => {
-    console.log(e.target);
-    action();
-}
+    let newFilm = inputField.value;
+    const isFavourite = checkbox.checked;
 
-document.querySelector('.header__logo').addEventListener('click', deleteListener);
-// document.querySelector('.header__logo').removeEventListener('click', deleteListener);
+    if (newFilm) {
 
+      if (newFilm.length > 21) {
+        newFilm = `${newFilm.substring(0, 22)}...`;
+      } 
 
+      if (isFavourite) {
+        console.log('Add favourite film');
+      }
+      
+      movieDB.movies.push(newFilm);
+      sortArr(movieDB.movies);
 
-function action() {
-    annoyingAd.forEach((e) => {
-        e.remove();
-    })
-    
+      createMoviesList(movieDB.movies, listOfFilms);
+    }
+
+    ev.target.reset();
+  });
+
+  const deleteAD = function(arr) {
+    arr.forEach((e) => {
+      e.remove();
+    });
+  };
+
+  // deleteAD(annoyingAd); function call is transfered down into united ordered calls' list
+
+  const makeChanges = () => {
     genre.textContent = 'Drama';
     bg.style.cssText = 'background-image:url("img/bg.jpg")';
-    
-    movieDB.movies.sort();
-    // console.log(movieDB.movies);
-    
-    listOfFilms.innerHTML = '';
-    
-    movieDB.movies.forEach((film, i) => {
-        listOfFilms.innerHTML += `
-        <li class="promo__interactive-item">${i+1}) ${film}
-        <div class="delete"></div>
-        </li>
-        `
-    })
-}
+  };
 
+  // makeChanges(); // function call is transfered down into united ordered calls' list
+
+  const sortArr = (arr) => {
+    arr.sort();
+  };
+  
+  // sortArr(movieDB.movies); function call is transfered down into united ordered calls' list
+  
+  function createMoviesList(films, parent) {
+    parent.innerHTML = '';
+
+    sortArr(films);
+    
+    films.forEach((film, i) => {
+      parent.innerHTML += `
+      <li class="promo__interactive-item">${i+1}) ${film}
+      <div class="delete"></div>
+      </li>
+      `
+    });
+
+    document.querySelectorAll('.delete').forEach((basket, i) => {
+      basket.addEventListener('click', () => {
+        basket.parentElement.remove();
+        movieDB.movies.splice(i, 1);
+
+        createMoviesList(films, parent);
+      });
+    });
+  }
+
+  deleteAD(annoyingAd);
+  makeChanges();
+  createMoviesList(movieDB.movies, listOfFilms);
+});
