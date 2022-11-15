cd6a4ccd5fc25de16c246350e9d05f47ccf7c5a5
Mahinich <andrey.mahinitch@gmail.com>
2022-11-16 00:41:56 +0200

    54 is upworked

--- 54 optional chaininbg/script.js
+++ 54 optional chaininbg/script.js
@@ -2,5 +2,29 @@
 
 const box = document.querySelector('.box');
 const block = document.querySelector('.block');
+const clock = null;
 
-console.log(block)
+// console.log(block);
+
+/* if (block) {
+  console.log(block.textContent);
+} */
+
+// block?.textContent = "123";
+
+const userData = {
+  name: 'Andriy',
+  age: null,
+  say: () => {
+    console.log('Hello');
+  }
+};
+
+userData.say();
+userData.hey?.();
+
+/* if (userData && userData.skills && userData.skills.js ) {
+  console.log(userData.skills.js);
+} */
+
+console.log(userData?.skills?.js);
