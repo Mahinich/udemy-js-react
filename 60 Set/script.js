5492087679579f8912a65703ec5b6294e635a10f
Mahinich <andrey.mahinitch@gmail.com>
2022-11-23 21:14:00 +0200

    60 is upworked

--- 60 Set/script.js
+++ 60 Set/script.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const arr = ['Natali', 'Anton', 'Polina', 'Natali'];
+
+function unique(arr) {
+  return Array.from(new Set(arr));
+}
+
+console.log(unique(arr));
+// const set = new Set(arr);
+
+// set.add('Ivan')
+//    .add('Anton');
+
+// console.log(set);
+
+// set.delete(value);
+// set.has(value);
+// set.size;
+// set.clear();
+
+/* for (let value of set) {
+  console.log(value);
+} */
+
+/* set.forEach((value, valueAgain, set) => {
+  console.log(value, valueAgain);
+}); */
+
+// console.log(set.values());
+// console.log(set.keys());
+// console.log(set.entries());
