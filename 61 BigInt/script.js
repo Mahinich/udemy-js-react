3239af7b9beb01f31469d1d7001c23763411942a
Mahinich <andrey.mahinitch@gmail.com>
2022-11-24 21:51:22 +0200

    61 is upworked

--- 61 BigInt/script.js
+++ 61 BigInt/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+// const bigint = 22323890013400814098170934913490980341981341340324134n;
+
+const sameBigint = BigInt(22323890013400814098170934913490980341981341340324134);
+
+console.log(5n === 5);
+
+const bigint = 1n;
+let num = 2;
+
+console.log(bigint + BigInt(num));
+
+console.log(Number(bigint) + num);
