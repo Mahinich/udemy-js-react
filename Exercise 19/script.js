1b796e50d13b169f608fba2f01338a6b3d931746
Mahinich <andrey.mahinitch@gmail.com>
2022-11-27 14:48:21 +0200

    ex 19 is partly upworked. Only appears som eunderstanding about reduce method

--- Exercise 19/script.js
+++ Exercise 19/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const arr = [1, 2, 3, 4, 5, [1, 2, 3]];
+
+/* function deepCount(data) {
+  let count = data.length;
+
+  for (let i = 0; i < data.length; i++) {
+    if (Array.isArray(data[i])) {
+      count += deepCount(data[i]);
+    }
+  }
+
+  return count;
+}
+
+console.log(deepCount(arr)); */
+
+function deepCount(a){
+  return a.reduce((s,e)=>s+(Array.isArray(e) ? deepCount(e) : 0),a.length);
+}
+
+console.log(deepCount(arr));
