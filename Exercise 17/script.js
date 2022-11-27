2f96fcfa94e3e9fae438cd2e806febe98daf30ac
Mahinich <andrey.mahinitch@gmail.com>
2022-11-27 10:38:28 +0200

    ex 17 is upworked

--- Exercise 17/script.js
+++ Exercise 17/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function amountOfPages(summary){
+  let result = '';
+  let n = 0;
+
+  for (let i = 1; i <= summary; i++) {
+   result += i;
+    if (result.length === summary) {
+      n = i;
+      break;
+    }
+  }
+  return n;
+}
+
+const pagesAmount = amountOfPages(25);
+
+console.log(pagesAmount);
