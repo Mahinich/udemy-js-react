b831f7db18c9a0535b71f616e9340b656616d946
Mahinich <andrey.mahinitch@gmail.com>
2023-02-08 09:11:28 +0200

    created a separated Food_dist folder to instal nom packages

---
+++ Food_dist/js/script.js
@@ -0,0 +1,308 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  //TABS
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabsContent() {
+      tabsContent.forEach( item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+      });
+
+      tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+      });
+    }
+
+    function showTabsContent(i = 0) {
+      tabsContent[i].classList.add('show', 'fade');
+      tabsContent[i].classList.remove('hide');
+      tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', (e) => {
+      const target = e.target;
+      if (target && target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, i) => {
+          if (target == item) {
+            hideTabsContent();
+            showTabsContent(i);
+          }
+        });
+      }
+    });
+  
+  // TIMER
+    
+  const deadline = '2023-01-31';
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    
+    if(t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+      minutes = Math.floor((t / 1000 / 60) % 60 ),
+      seconds = Math.floor((t / 1000) % 60);
+      }
+    return {
+      total: t,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function getZero(num) {
+    if(num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = document.querySelector('#days'),
+          hours = document.querySelector('#hours'),
+          minutes = document.querySelector('#minutes'),
+          seconds = document.querySelector('#seconds');
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if(t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
+
+  // MODAL
+
+  const modalOpen = document.querySelectorAll('[data-modal]');
+  const modal = document.querySelector('.modal');
+  // const closeModal = document.querySelector('[data-close]');
+
+  function showModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+  
+  modalOpen.forEach((item) => {
+    item.addEventListener('click', showModal);
+  });
+  
+  function hideModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+  
+  // closeModal.addEventListener('click', hideModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
+      hideModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      hideModal(); 
+    }
+  });
+
+  //MODAL MODIFICATION 
+
+  const modalTimerId = setTimeout(showModal, 50000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= 
+      document.documentElement.scrollHeight - 1) {
+        showModal();
+        window.removeEventListener('scroll', showModalByScroll);
+      }
+  }
+  
+  window.addEventListener('scroll', showModalByScroll);
+
+  // Use classes for cards
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.course = 40;
+      this.changeCurrency();
+    }
+    changeCurrency() {
+      this.price = this.price * this.course;
+    }
+    render() {
+      const element = document.createElement('div');
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container',
+   
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    10,
+    '.menu .container',
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    11,
+    '.menu .container',
+  ).render();
+
+  // Forms to server 
+
+  const forms = document.querySelectorAll('form');
+
+  const message = { 
+    success: 'Дякуємо, дані відправлені',
+    loading: 'img/forms/spinner.svg',
+    error: 'Виникла помилка',
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  });
+    
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+      display: block;
+      margin: 0 auto;
+      `;
+      // form.append(statusMessage);
+      form.insertAdjacentElement('afterend', statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      // request.setRequestHeader('Content-type', 'multipart/form-data'); // 1(formData format send) Do not set Header while using in conjunction XHR and formData. formData object does this itselves. If to set up it manually, we won't see a response from server. It shows array(0) in the console.
+      request.setRequestHeader('Content-type', 'application/json'); // 2 (json format send) use headers if we need to send data in JSON format
+      const formData = new FormData(form);
+
+      const obj = {}; // 2 (json format send) use this structure to prepear for converting formData object to JSON
+      formData.forEach(function(value, key) { //2 (json format send)
+        obj[key] = value; //2 (json format send)
+      }); //2json
+
+      const json = JSON.stringify(obj); // 2 (json format send)
+      
+      request.send(json); // 2 (json format send) use this to send data in JSON format
+      // request.send(formData); // 1(formData format send) use formData object if to send data in XHR format
+
+      // const resetValue= form.querySelectorAll('input'); //variable for alternative variant instead of form.reset();
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          showThanksModal(message.success);
+          form.reset();
+          statusMessage.remove();
+
+         /*  resetValue.forEach(elem => { // alternative variant instead of form.reset();
+            elem.value = '';
+          }) */
+        } else {
+          console.log(request.response);
+          showThanksModal(message.error);
+        }
+      });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    showModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('.modal__dialog');
+    thanksModal.innerHTML = `
+    <div class='modal__content'> 
+      <div class='modal__close' data-close></div>
+      <div class='modal__title'>${message}</div>
+    </div>
+    `;
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      hideModal();
+    }, 4000);
+  }
+});
